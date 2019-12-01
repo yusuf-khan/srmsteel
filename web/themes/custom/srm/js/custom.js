3c80d29be9e79c05d815b02eb24c703cb5c28694
yusuf-khan <khany762@gmail.com>
2019-12-01 13:49:56 +0530

    theme changes

---
+++ web/themes/custom/srm/js/custom.js
@@ -0,0 +1,8 @@
+
+jQuery(document).ready(function($){
+
+console.log(123);
+        // $(.nav-div .nav-list-item a).addClass('nav-list-item-link');
+        $('.nav-div ul li').find('a').addClass('nav-list-item-link');
+
+});
